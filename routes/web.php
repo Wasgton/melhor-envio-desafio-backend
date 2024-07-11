7c9b4f28d21a47620eba1f3a23e0b92b5ddf062a
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2024-07-11 19:09:54 -0300

    feat: routes
    Creating routes for reports

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/teste', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => '/report', 'controller' => ReportController::class], static function () {
+    Route::get('/consumer', 'exportConsumerRequests');
+    Route::get('/service', 'exportServiceRequests');
+    Route::get('/latency', 'exportAverageTimePerService');
 });
