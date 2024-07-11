a0c2d4a92d72c822a95e628f6168223f33e4c8c2
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2024-07-11 16:52:18 -0300

    tests: refactoring controller tests

--- tests/Feature/Http/Controllers/ImportFileControllerTest.php
+++ tests/Feature/Http/Controllers/ImportFileControllerTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Jobs\ImportLogJob;
 use Illuminate\Http\Response;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
@@ -14,16 +16,29 @@ class ImportFileControllerTest extends TestCase
         $response = $this->json('POST', 'api/import-file', []);
         $response->assertJsonValidationErrorFor('file');
     }
-    
+
     public function test_should_upload_file_and_return_ok(): void
     {
-        $fileFromDisk = Storage::disk()->get('json_file.txt');
-        $file = UploadedFile::fake()->create('file.txt', 100, 'text/plain', $fileFromDisk);
-        Storage::fake('json_file');
-        $response = $this->json('POST', 'api/import-file', [
-            'file' => $file 
+        Queue::fake();
+        $file = UploadedFile::fake()->create('test.txt', 5 * 1024);
+        $response = $this->json('POST', '/api/import-file', [
+            'file' => $file,
         ]);
-        $response->assertStatus(Response::HTTP_OK);
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Import queued successfully.']);
+        Queue::assertPushed(ImportLogJob::class);
     }
 
+    public function test_should_return_exception()
+    {
+        Queue::fake();
+        $file = UploadedFile::fake()->create('test.txt', 5 * 1024);
+        $response = $this->json('POST', '/api/import-file', [
+            'file' => $file,
+        ]);
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Import queued successfully.']);
+        Queue::assertPushed(ImportLogJob::class);
+    }
+    
 }
